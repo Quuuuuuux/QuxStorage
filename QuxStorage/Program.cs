26183bc38ac3570abf40a6fe88adc3781ed51ead
a.shunyaev <av.shunaev.qux@gmail.com>
2022-08-31 00:03:14 +0300

    feat: add config service and test config

--- QuxStorage/Program.cs
+++ QuxStorage/Program.cs
@@ -1,6 +1,18 @@
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Utils.Configurations;
+using Utils.Configurations.ConfigurationClasses;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<KestrelServerOptions>(x => { x.Limits.MaxRequestBodySize = 1073741824; });
+
+builder.Host.ConfigureAppConfiguration((_, config) =>
+{
+    config.AddConfigFiles(new TestConfig());
+    config.AddEnvironmentVariables();
+}).ConfigureServices((hostConfig, config) => { config.BindConfigFiles(hostConfig.Configuration); });
+
+
 var app = builder.Build();
-
 app.MapGet("/", () => "Hello World!");
-
 app.Run();
---
+++ Utils/Configurations/ConfigurationClasses/TestConfig.cs
@@ -0,0 +1,6 @@
+namespace Utils.Configurations.ConfigurationClasses;
+
+public class TestConfig
+{
+    public string Test { get; set; } = string.Empty;
+}
